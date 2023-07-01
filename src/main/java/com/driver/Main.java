5bc642fc5d74b6eac63094b8889cc2af3575103c
Tumwesigye Ronnie <tumronald1@gmail.com>
2023-07-01 11:14:54 +0300

    Method Overloading, implementing polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(3,4));
+        System.out.println(p.product(4,5,7));
+        System.out.println(p.product(3.4, 5));
+    }
 
+}
+class Product {
+public int product(int x, int y){
+    return x * y;
+}
+public  int product(int x, int y, int z){
+    return x * y * z;
+}
+
+public double product(double x, double y){
+    return x * y;
+}
 }
